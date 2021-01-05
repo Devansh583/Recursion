8b0c686b1c9c1d7129f17e517d9470aee4154d01
Devansh583 <60072630+Devansh583@users.noreply.github.com>
2021-01-05 21:41:14 +0530

    Create remove_x.cpp

---
+++ remove_x.cpp
@@ -0,0 +1,30 @@
+#include<iostream.h>
+using namespace std;
+
+//defining the function remove_x  
+string remove_x(string s, int length)
+{
+    if(length==0)
+        return "";
+
+    char ch= s[0];
+
+    //update the string
+    string temp= remove_x(s.substr(1), length-1);
+
+    //if the char are same
+    if(s[0]=='x')
+        return temp+'x';
+
+    return ch+temp;
+}
+int main(void)
+{
+    //input the string
+    string s="";
+    cout<<"Enter the string: ";
+    cin>>s;
+
+    cout<<"String on modification: "<<remove_x(s, s.length());
+
+}
